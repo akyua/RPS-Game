78ba19fd2dee09b8f07ace8768be0a2794f2b56b
Gabriel <gabrielnovatoo@gmail.com>
2023-04-05 17:18:57 -0300

    Function with choices, the game and all messages dones

--- script.js
+++ script.js
@@ -1,55 +1,115 @@
+function choice(arr){
+    let playerChoice = arr;
+    document.getElementById('user_choice').innerHTML = `You choose: <span>${playerChoice}</span>`;
+    const userScore = document.getElementById("user-score");
+    const computerScore = document.getElementById("computer-score");
+    
+    let computerChoice = Math.random();
+    if(computerChoice < 0.34){
+        computerChoice = "Rock";
+    } else if(computerChoice <= 0.67){
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissor";
+    }
+    document.getElementById('computer_choice').innerHTML = `Computer choose: <span>${computerChoice}</span>`;
+
+    function game(playerChoice, computerChoice){
+        let resultGame = " ";
+        if(playerChoice === computerChoice){
+            resultGame = 'Draw';
+        }
+    
+        if(playerChoice === "Rock"){
+            if(computerChoice === "Scissor"){
+                resultGame = 'Win';
+            } else if (computerChoice === "Paper"){
+                resultGame = 'Lose';
+            }
+        }
+    
+        if(playerChoice === "Scissor"){
+            if(computerChoice === "Rock"){
+                resultGame = 'Win';
+            } else if (computerChoice === "Paper"){
+                resultGame = 'Lose';
+            }
+        }
+    
+        if(playerChoice === "Paper"){
+            if(computerChoice === "Rock"){
+                resultGame = 'Win';
+            } else if (computerChoice === "Scissor"){
+                resultGame = 'Lose';
+            }
+        }
+        document.getElementById('result').innerHTML = `You <span>${resultGame}</span>`;
+        if(resultGame === 'Win'){
+            userScore.innerHTML++;
+        } else if(resultGame === 'Lose'){
+            computerScore.innerHTML++;
+        }
+    }
+    game(playerChoice, computerChoice);
+
+}
+/*  Oldest Code
+
 function getComputerChoice() {
     let computerChoice = Math.random();
     if(computerChoice < 0.34){
-        computerChoice = "rock";
+        computerChoice = "Rock";
     } else if(computerChoice <= 0.67){
-        computerChoice = "paper";
+        computerChoice = "Paper";
     } else {
-        computerChoice = "scissor";
+        computerChoice = "Scissor";
     }
+    document.getElementById('computer_choice').innerHTML = `Computer choose: <span>${computerChoice}</span>`
     return(computerChoice);
 }
+
+function getPlayerChoice(arr){
+    
+}
+
 
 function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         return "The result is a tie!";
     }
 
-    if(playerSelection === "rock"){
-        if(computerSelection === "scissor"){
+    if(playerSelection === "Rock"){
+        if(computerSelection === "Scissor"){
             return "You win!";
-        } else if (computerSelection === "paper"){
+        } else if (computerSelection === "Papper"){
             return "You lose!"; 
         }
     }
 
-    if(playerSelection === "scissor"){
-        if(computerSelection === "rock"){
+    if(playerSelection === "Scissor"){
+        if(computerSelection === "Rock"){
             return "You win!";
-        } else if (computerSelection === "paper"){
+        } else if (computerSelection === "Papper"){
             return "You lose!";
         }
     }
 
-    if(playerSelection === "paper"){
-        if(computerSelection === "rock"){
+    if(playerSelection === "Papper"){
+        if(computerSelection === "Rock"){
             return "You win!";
-        } else if (computerSelection === "scissor"){
+        } else if (computerSelection === "Scissor"){
             return "You lose!";
         }
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice(); 
+const playerSelection = getPlayerChoice();
+/* const computerSelection = getComputerChoice(); */
 
-console.log("Your Choice: " + playerSelection);
-console.log("Computer Choice: " + computerSelection);
+/* playRound(playerSelection, computerSelection) */
 
-console.log(playRound(playerSelection, computerSelection));
-
-function game(playRound){
+/* function game(playRound){
     for(let i = 0; i < 5; i++){
         
     }
-}
+}  */
